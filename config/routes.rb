99c171e80f6c143ca6bc942cda745c109a67184b
ryochin33 <1210.ryo.i@gmail.com>
2024-06-17 18:00:05 +0900

    新規チャットルーム作成機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'rooms/index'
   devise_for :users
   root to: 'tops#index' # トップページをルートに設定
-  resources :rooms  
+  resources :rooms, only: [:index, :new, :create] 
 end
