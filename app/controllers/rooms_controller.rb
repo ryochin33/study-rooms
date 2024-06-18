f6d6b9744f15d2c5369db6ba9a87d38df2f57293
ryochin33 <1210.ryo.i@gmail.com>
2024-06-18 17:15:44 +0900

    チャットルーム一覧（バックエンド）

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,9 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @rooms = Room.all
+    @room = Room.first
+    @messages = @room ? @room.messages.includes(:user).order(created_at: :desc) : []
   end
 
   def new
@@ -15,6 +18,12 @@ class RoomsController < ApplicationController
     else
       render :new
   end
+end
+
+def destroy
+  @room = Room.find(params[:id])
+  @room.destroy
+  redirect_to rooms_path
 end
 
   private
